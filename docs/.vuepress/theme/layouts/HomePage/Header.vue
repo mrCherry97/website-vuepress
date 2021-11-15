c1496c489e5cbb7e0df6f1172dc65f5a1b85a677
mwisnia97 <mwisnia97@gmail.com>
2021-11-15 11:06:10 +0100

    Removed unnecesary transition

--- docs/.vuepress/theme/layouts/HomePage/Header.vue
+++ docs/.vuepress/theme/layouts/HomePage/Header.vue
@@ -85,27 +85,6 @@
                 </svg>
               </a>
             </div>
-
-            <transition
-              enter-active-class="transition ease-out duration-200"
-              enter-from-class="opacity-0 translate-y-1"
-              enter-to-class="opacity-100 translate-y-0"
-              leave-active-class="transition ease-in duration-150"
-              leave-from-class="opacity-100 translate-y-0"
-              leave-to-class="opacity-0 translate-y-1"
-            >
-            </transition>
-          </Popover>
-          <Popover class="relative">
-            <transition
-              enter-active-class="transition ease-out duration-200"
-              enter-from-class="opacity-0 translate-y-1"
-              enter-to-class="opacity-100 translate-y-0"
-              leave-active-class="transition ease-in duration-150"
-              leave-from-class="opacity-100 translate-y-0"
-              leave-to-class="opacity-0 translate-y-1"
-            >
-            </transition>
           </Popover>
         </PopoverGroup>
       </div>
